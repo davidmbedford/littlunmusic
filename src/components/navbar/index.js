723767ca59478fbcc601897b79509cc03c28aaf4
davidmbedford <david.m.bedford1@gmail.com>
2020-01-14 16:45:47 -0500

    Appjs built with routing

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -20,7 +20,7 @@ class Navbar extends Component {
               <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><Link to='/'>Home</Link></li>
-                <li><Link to='/gear'>Bio</Link></li>
+                <li><Link to='/biography'>Bio</Link></li>
                 <li><Link to='/discography'>Discography</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
               </ul>
@@ -28,14 +28,6 @@ class Navbar extends Component {
           </nav>
     
             <ul id="slide-out" className="sidenav">
-              {/* <li><div class="user-view">
-                <div class="background">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/7/77/Big-eared-townsend-fledermaus.jpg" />
-                </div>
-                <a href="#user"><img class="circle" src="images/yuna.jpg" /></a>
-                <a href="#name"><span class="white-text name">John Doe</span></a>
-                <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
-              </div></li> */}
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/gear'>Bio</Link></li>
                 <li><Link to='/discography'>Discography</Link></li>
