3e6904c7416885200805fce174e47fdce9fe542e
davidmbedford <david.m.bedford1@gmail.com>
2020-01-15 17:05:25 -0500

    embedded player successfully added to the discography page

--- src/screens/discography/index.js
+++ src/screens/discography/index.js
@@ -8,6 +8,14 @@ class Discography extends Component {
             <div className="Jumbotron">
             <h1>This is the Discography page</h1>
             </div>
+            <div>
+            <iframe src="https://bandcamp.com/EmbeddedPlayer/track=417987299/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/artwork=small/transparent=true/" seamless>
+                <a href="http://littlun.bandcamp.com/track/forest-circles">forest circles by littlun</a></iframe>
+            <iframe src="https://bandcamp.com/EmbeddedPlayer/track=1093888758/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/artwork=small/transparent=true/" seamless>
+                <a href="http://littlun.bandcamp.com/track/house-of-ghosts">House of Ghosts by littlun</a></iframe>
+            <iframe src="https://bandcamp.com/EmbeddedPlayer/album=3689248723/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/artwork=small/transparent=true/" seamless>
+                <a href="http://littlun.bandcamp.com/album/sentient-youth">Sentient Youth by littlun</a></iframe>
+            </div>
             </>
         )
     }
