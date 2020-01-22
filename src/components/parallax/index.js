d68368977d4045e5e9eb5afc8ce7e8cc53ad983b
davidmbedford <david.m.bedford1@gmail.com>
2020-01-21 23:39:19 -0500

    parallax functionality working properly

--- src/components/parallax/index.js
+++ src/components/parallax/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import grave from '../../assets/images/graveyard.jpg';
 import './style.css';
 
-function Parallax() {
+class Parallax extends Component {
+    componentDidMount() {
+    document.addEventListener('DOMContentLoaded', function() {
+        const M = window.M;
+        var elems = document.querySelectorAll('.parallax');
+        var instances = M.Parallax.init(elems, {});
+      });
+    }
+    
+    render() {
         return (
           <>
-            <div className="container-fluid parallax">
-                <p>...</p>
+            {/* <div className="container-fluid parallax">
+               <img className="headImage" src={grave}></img>
+            </div> */}
+
+            <div className="parallax-container">
+                <div className="parallax">
+                    <img className="headImage" src={grave}></img>
+                </div>
             </div>
           </>
         )
+        }
     };
 
 export default Parallax;
