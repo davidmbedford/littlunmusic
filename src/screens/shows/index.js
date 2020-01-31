eb4ec4e13f2fde1ca3c8651ab28a78e84526380a
davidmbedford <david.m.bedford1@gmail.com>
2020-01-31 00:41:13 -0500

    links added to show info

--- src/screens/shows/index.js
+++ src/screens/shows/index.js
@@ -10,7 +10,7 @@ function Shows() {
             <br/>
             <div className="container">
               <h3>February 15, 2019</h3>
-              <h5>World Cafe Live (Philadelphia, PA)</h5>
+              <h5><a href="https://www.facebook.com/events/2705375342839601/">World Cafe Live (Philadelphia, PA)</a></h5>
               <p>w/ Tubey Frank and Lylyly</p>
               <p>9 pm || $10 </p>
             </div>
