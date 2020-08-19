8bf1a096a23d5c356aa3f3b0ac85390c6918202e
davidmbedford <david.m.bedford1@gmail.com>
2020-08-19 11:15:45 -0400

    discog page updated

--- src/screens/discography/index.js
+++ src/screens/discography/index.js
@@ -9,24 +9,35 @@ function Discography() {
           <div className="container">
             <h1>Discography</h1>
             <div className="container center discogbox">
+
+              <div className="row center">
+                <p><span className="strong">Album Name:</span> Pandemic Dream Journal || <span className="strong">Release Date:</span> Jun. 2020 </p>
+                <iframe src="https://bandcamp.com/EmbeddedPlayer/album=1425014592/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless>
+                  <a href="http://littlun.bandcamp.com/album/pandemic-dream-journal">pandemic dream journal by littlun</a>
+                </iframe>
+              </div>
+
               <div className="row center">
                 <p><span className="strong">Album Name:</span> Fallow (single) || <span className="strong">Release Date:</span> Mar. 2019 </p>
                 <iframe src="https://bandcamp.com/EmbeddedPlayer/track=417987299/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/artwork=small/transparent=true/" seamless>
                   <a href="http://littlun.bandcamp.com/track/forest-circles">forest circles by littlun</a>
                 </iframe>
               </div>
+
               <div className="row center">
                 <p><span className="strong">Album Name:</span> House of Ghosts || <span className="strong">Release Date:</span> Jan. 2019 </p>
                 <iframe src="https://bandcamp.com/EmbeddedPlayer/track=1093888758/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/artwork=small/transparent=true/" seamless>
                   <a href="http://littlun.bandcamp.com/track/house-of-ghosts">House of Ghosts by littlun</a>
                 </iframe>
               </div>
+
               <div className="row center">
                 <p><span className="strong">Album Name:</span> Sentient Youth || <span className="strong">Release Date:</span> Oct. 2018 </p>
                 <iframe src="https://bandcamp.com/EmbeddedPlayer/album=3689248723/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/artwork=small/transparent=true/" seamless>
                   <a href="http://littlun.bandcamp.com/album/sentient-youth">Sentient Youth by littlun</a>
                 </iframe>
               </div>
+
             </div>
             <br />
           </div>
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -9,7 +9,6 @@ function Home() {
         <div className="container bio">
           <img src={headshot} className="headshot" alt="logo" />
           <p> This is the home of littlun, a band of loving young multi-instrumentalists led by singer/songwriter Molly Lynn White.</p>
-          <p> Welcome. Here you can stream their music, find upcoming shows, and reach out to them for booking requests. </p>
           <p> Molly has been writing under the name littlun for a few years now. Their music features
           classically influenced melodies and dark folk motifs. A full band formed around them in 2019
           to fill out the new songs. You can catch littlun play at local venues in Philly such as
